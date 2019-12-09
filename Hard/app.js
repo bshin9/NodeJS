cee32dfe9c3de52aa373c6e5c1b1b98398b6de31
Brian Shin <bshin9@yahoo.com>
2019-12-09 12:02:33 -0500

    added node challenge explanation

--- Hard/app.js
+++ Hard/app.js
@@ -1,91 +1,99 @@
+// HARD: Add the remaining CRUD functionality to your medium problem.
+// Make sure you return the proper HTTP status codes based on the outcome of the request. Be sure to implement error checking here.
+// If an invalid request is made, we want to return some sort of error message and the correct HTTP status code for the situation.
+// HTTP Status Codes: http://www.restapitutorial.com/httpstatuscodes.html
+
 // http://localhost:3000/api/hard postman link
 
-const Joi = require('joi');
-const express = require ('express');
+const Joi = require("joi");
+const express = require("express");
 const app = express();
-const data = require('./employees.json')
+const data = require("./employees.json");
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('Hello what');
+app.get("/", (req, res) => {
+  res.send("Hello what");
 });
 
-app.get('/api/hard', (req, res) => {
-    res.send(data);
+app.get("/api/hard", (req, res) => {
+  res.send(data);
 });
 
-// post method will let us post new information to our server but needs to be input correctly 
+// post method will let us post new information to our server but needs to be input correctly
 // if not correct then it will return a 400 error
 
-app.post('/api/hard', (req, res) => {
-    const { error } = validateEmployee(req.body);
-    if (error) { return res.status(400).send(error.details[0].message)
-    }
+app.post("/api/hard", (req, res) => {
+  const { error } = validateEmployee(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-    const employee = {
-        id: data.employees.length + 1,
-        name: req.body.name,
-        salary: req.body.salary,
-        department: req.body.department
-    };
-    data.employees.push(employee);
-    res.send(employee);
+  const employee = {
+    id: data.employees.length + 1,
+    name: req.body.name,
+    salary: req.body.salary,
+    department: req.body.department
+  };
+  data.employees.push(employee);
+  res.send(employee);
 });
 
 // put method allows us to update employee by id since we're using :id
 
-app.put('/api/hard/:id', (req, res) => {
-    const employee = data.employees.find(function(employee){
-        return parseInt (req.params.id) === employee.id 
-    })
-    if (!employee){ res.status(404).send('Employee cannot be found')
-}
-    res.send(employee);
-    
-    const { error } = validateEmployee(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message)
-        return;
-    }
+app.put("/api/hard/:id", (req, res) => {
+  const employee = data.employees.find(function(employee) {
+    return parseInt(req.params.id) === employee.id;
+  });
+  if (!employee) {
+    res.status(404).send("Employee cannot be found");
+  }
+  res.send(employee);
 
-    employee.name = req.body.name;
-    res.send(employee);
+  const { error } = validateEmployee(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  employee.name = req.body.name;
+  res.send(employee);
 });
 
 // delete will allow us to delete employees by id
 // using the Joi.string to make sure min length of 3 and then return employee and schema values
 
-app.delete('/api/hard/:id', (req, res) => {
+app.delete("/api/hard/:id", (req, res) => {
+  const employee = data.employees.find(c => c.id === parseInt(req.params.id));
+  console.log(employee);
+  if (!employee) {
+    res.status(404).send("Employee cannot be found");
+  }
 
-    const employee = data.employees.find(c => c.id === parseInt(req.params.id))
-    console.log(employee)
-    if (!employee){ 
-        res.status(404).send('Employee cannot be found')
-    }
-
-    const index = data.employees.indexOf(employee);
-    data.employees.splice(index, 1);
-    res.send(employee);
+  const index = data.employees.indexOf(employee);
+  data.employees.splice(index, 1);
+  res.send(employee);
 });
 
 function validateEmployee(employee) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
 
-    return Joi.validate(employee, schema);
+  return Joi.validate(employee, schema);
 }
 
 // used the if statement in every method in order to return the errors when the correct information is not input
 
-app.get('/api/hard/:id', (req, res) => {
-    const employee = data.employees.find(function(employee){
-        return parseInt (req.params.id) === employee.id 
-    })
-    if (!employee){ res.status(404).send('Employee cannot be found')
-}
-    res.send(employee);
+app.get("/api/hard/:id", (req, res) => {
+  const employee = data.employees.find(function(employee) {
+    return parseInt(req.params.id) === employee.id;
+  });
+  if (!employee) {
+    res.status(404).send("Employee cannot be found");
+  }
+  res.send(employee);
 });
 
 const port = process.env.PORT || 3000;
